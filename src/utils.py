0a3e273116b09b57374a9859e5498fe7ccb0b85c
Odilbek99 <umaralievodili@gmail.com>
2023-05-21 00:38:18 +0300

    add hyperparameters

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -19,14 +20,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
-            
-            model.fit(x_train,y_train)
+            par = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,par,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train )
+
+        
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
