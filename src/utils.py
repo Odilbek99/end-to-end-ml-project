03223dd53bd75cc44ac0707744be23ac410ea198
Odilbek99 <umaralievodili@gmail.com>
2023-05-21 00:01:49 +0300

    Update evaluate models function

--- src/utils.py
+++ src/utils.py
@@ -28,16 +28,17 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
             
             model.fit(x_train,y_train)
 
-            x_train_pred = model.predict(x_train)
-            y_train_pred = model.predict(y_train)
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
 
             train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
         return report
     
-    except:
-        pass
+    except Exception as e:
+        raise CustomException(e, sys)
+
 
 
