6d7e13d1ca75ec726dcfe95b65da6dea4bc05c2e
Odilbek99 <umaralievodili@gmail.com>
2023-05-23 09:47:24 +0300

    update

--- application.py
+++ application.py
@@ -6,15 +6,14 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 application=Flask(__name__)
 
-app = application
 
 # Route for a home page
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET','POST'])
+@application.route('/predict', methods=['GET','POST'])
 def predict_datapoint():
     if request.method == "GET":
         return render_template('home.html')
@@ -37,6 +36,6 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
     
 if __name__=="__main__":
-    app.run()
+    application.run()
      
 
